26c9bd44f3931562dda3a2bf232253f82b8f2c66
Mia Dao <daouyenmy@live.com>
2021-08-17 20:49:45 -0400

    we started the project! yayagit status!

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App =()=> {
+    <h1> We did something today </h1>
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
