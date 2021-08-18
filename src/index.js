0293e8fd225bb1419ee559eb7d7c07e21f509059
Mia Dao <daouyenmy@live.com>
2021-08-17 21:30:19 -0400

    because I have to write a message so here's the message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router,
+Link,
+Route
+}from 'react-router-dom';
 
 const App =()=> {
     <h1> We did something today 
         <div>Dan added this.</div>
     </h1>
+    return (
+        <Router>
+        </Router>
+    )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
