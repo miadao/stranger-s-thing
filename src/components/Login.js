a1a3659b1111ffb56d6e4d39c18a7c3a215df715
Mia Dao <daouyenmy@live.com>
2021-08-21 09:32:31 -0400

    still sleeping

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,42 @@
-const Login = () => {
-    return 'a';
+import { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
+const Login = (setAuthenticated) => {
+    const {LoginSucess, setLoginSuccess} = useState(false);
+    function authenticate(event){
+        event.preventDefault();  
+
+        //LOGIC
+        //check for inputs
+        //validate the username and password
+        //make AJAX request to server, then server will send back response if user is authenticated 
+        //if authenticated, re-directed to home page
+        //if not authenicated, display the message to user's screen 
+        
+        console.log('form submitted')
+        setLoginSuccess(true);
+        
+    }
+
+    if(LoginSucess){
+        return <Redirect to="/Posts"/>
+    } return (
+        <section className="login"> 
+            <h1>Login</h1>
+            <form onSubmit={authenticate}>
+                <div>
+                    <label>Username: </label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="text"/>
+                </div>
+                <button type="submit">Login</button>
+                <Link className="LinkBtn" to="/register">Register</Link>
+            </form>
+            </section>
+    )
 }
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router,
+Route,
+Link,
+Switch
+} from 'react-router-dom';
 
 
 import {
@@ -20,6 +25,7 @@ const App =()=> {
 
     const [user, setUser] = useState({});
     const [posts, setPosts] = useState([])
+    const [authenticated, setAuthenticate] = useState(false);
    
    
     return <div className="app"> 
@@ -50,15 +56,41 @@ const App =()=> {
         <ViewProfile/>
 
         <CreateProfile
-            user={user}
-            setUser={setUser}
+            
         />
 
-
-   
-
-
-        
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li> <Link to="/">Home</Link></li>
+                        <li> <Link to="/">Posts</Link></li>
+                        <li> <Link to="/">Login</Link></li>
+                        <li> <Link to="/">Profile</Link></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/Posts">
+                        <Posts />
+                    </Route>
+                    <Route path="/Login">
+                        <Login 
+                            authenticated={authenticated}
+                            setAuthenticate={setAuthenticate}
+                        />
+                    </Route>
+                    <Route path="/Profile">
+                        <Profile />
+                    </Route>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="*">
+                        <h1>404 Error - Page Not Found!</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
         </div>
 }
 
