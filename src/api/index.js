164914264aa15f438af3e4cba884f64c14bc76ff
DanRD <danid92@vt.edu>
2021-08-21 12:26:00 -0400

    home apiindex index in public

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,11 @@
-export const BASE_URL ='https://strangers-things.herokuapp.com/api/2105-vpi-web-pt-class';
-fetch({BASE_URL})
-.then (res => res.json())
-.then(result => {
-    console.log(result)
-})
+export const BASE_URL ='https://strangers-things.herokuapp.com/api/2105-vpi-web-pt-class/posts';
+
+export async function fetchData() {
+    try {
+        const response = await fetch({BASE_URL});
+        const data = response.json();
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
