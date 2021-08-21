564a351efb24d416fedeed4c3c6b7b3255224042
Mia Dao <daouyenmy@live.com>
2021-08-21 14:39:53 -0400

    tried posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,46 @@
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState, Fragment } from 'react';
+
 
 const Posts = () => {
-    
+    const BASE_URL ='https://strangers-things.herokuapp.com/api/2105-vpi-web-pt-class'
+    const [posts, setPosts] = useState([]);
+  
 
-    return (
-        <div className="Posts">
+    useEffect (() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`${BASE_URL}/posts`);
+            const data = await response.json();
+            console.log("1", data) //see the structure of the posts data
+            console.log(posts)
+            setPosts(data);
+        }
+        fetchPosts();
+    }, [])
+    
+    return  <div className="Posts">
           
-            <section>
-                <h1>Posts</h1>
-            </section>
+        <section className="AllPosts">
+            <h1>Posts</h1>
+            {/* {
+                posts 
+                ?
+                posts.map((post) => 
+                    <Fragment key={post.id}>
+                        <span>{post.location}</span>
+                        <span>{post.willDeliver}</span>
+                        <span>{post.messages}</span>
+                        <span>{post.active}</span>
+                </Fragment>)
+                :
+                null
+            }   */}
+        </section>
             
-            <section> 
-                <label>Search: </label>
-                <input type="text"></input>
-                <button type="submit">Search</button>
+        <section> 
+            <label>Search: </label>
+            <input type="text"></input>
+            <button type="submit">Search</button>
 
                 {/* TO DO LIST*/}
                 {/* need onclick function, then read the data and list the posts upon user clicking, use Search component
@@ -25,15 +52,12 @@ const Posts = () => {
                 
                 Delete is just a button that wipes the data. Don't need this as a component?*/}
 
-            </section>
+        </section>
            
-           <section>
-                <Link to="/AddPost"><button className="AddPost">Add Post</button></Link>
-           </section>
-
-        </div>
-            
-    )
+        <section>
+            <Link to="/AddPost"><button className="AddPost">Add Post</button></Link>
+        </section>  
+    </div>
 }
 
 export default Posts;
