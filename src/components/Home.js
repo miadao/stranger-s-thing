d7eb0bda81b8e5cf38e2e633a66270d72322ae43
DanRD <danid92@vt.edu>
2021-08-21 11:06:29 -0400

    cowsay ET phone home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
-
-
-const Home = () => {
+const ViewProfile = () => {
+    const forViewProfile = () => {
+        console.log("Viewing Profile.")
+    }
     return <>
-    <h1>Welcome </h1>
+    <button onClick={forViewProfile}>VIEW PROFILE</button>
+    </>;
+}
+
+const Home = (/*{username}*/) => {
+    let props = { username: "username from API"}; // temporary. need info stored from API.
+    const {username} = props;
+    return <>
+    <h1>Welcome to Stranger's Things!</h1>
+    <h3>Loggin in as {username}</h3>
+    <ViewProfile />
             </>;
 }
 
