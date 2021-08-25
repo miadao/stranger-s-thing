d243540b3073085df93610a9cd2126d2e304a8c3
DanRD <danid92@vt.edu>
2021-08-24 20:48:14 -0400

    made most of AddPost

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,41 +1,50 @@
-const AddPost = () => {
+import React, {useState} from 'react';
+import { BASE_URL } from '../api';
 
-    const BASE_URL ='https://strangers-things.herokuapp.com/api/2105-vpi-web-pt'
-        fetch(`${BASE_URL}/posts/`, {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                posts: {
-                    "Title": `Title`,
-                    "Description": "password",
-                    "Price":"price",
-                    "Location":"location",
-                    "willDeliver":"true/false"
-                }
+const AddPost = ({post, setPost, title, setTitle, description, setDescription, price, setPrice, location, setLocation, willDeliver, setWillDeliver}) => {
+
+        const handleSubmit = async (event) => {
+            event.preventDefault();
+            const response = await fetch(`${BASE_URL}/posts`, {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'Application/json'
+                },
+                body: JSON.stringify({
+                    title,
+                    description,
+                    price,
+                    location,
+                    willDeliver
+                })
             })
-        }).then(res => res.json())
-          .then(result => console.log(result))
-          .catch(err => console.error(err));
+            const post = await response.json();
+            console.log("post: ", post);
+            setPost([post, ...post]);
+            setTitle('');
+            setDescription('');
+            setPrice('');
+            setLocation('');
+            setWillDeliver(null);
+        };
 
     return (
         <section className="AddNewPosts"> 
             <h1>Add New Posts</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label> Title: </label>
-                    <input type="text"/>
+                    <input type="text" value={title} onChange={(event)=>setTitle(event.target.value)}/>
                     <label> Description: </label>
-                    <input type="text"/>
+                    <input type="text" value={description}/>
                     <label> Price: </label>
-                    <input type="text"/>
+                    <input type="text" value={price}/>
                     <label> Location: </label>
-                    <input type="text"/>
-                    <input type="checkbox"/>
+                    <input type="text" value={location}/>
+                    <input type="checkbox" value={willDeliver}/>
                     <label>Willing to Deliver?</label>
+                    <button type="submit">Create Post</button>                    
                 </form>
-                <button type="submit">Create Post</button>
-            </section>
+        </section>
     ) 
 }
 
